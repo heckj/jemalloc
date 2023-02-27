1ec9d7d51e91382f96e936b3a18f9f7e319a66f8
Joe Heck <heckj@mac.com>
2023-02-27 19:25:16 +0000

    updating tests

--- test/_global/jemalloc.sh
+++ test/_global/jemalloc.sh
@@ -16,17 +16,7 @@ set -e
 # Optional: Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
-echo -e "The result of the 'color' command will be:\n"
-color
-echo -e "The result of the 'hello' command will be:\n"
-hello
-echo -e "\n"
-
-# Feature-specific tests
-# The 'check' command comes from the dev-container-features-test-lib.
-check "check purple is my favorite color" bash -c "color | grep 'my favorite color is purple'"
-check "check I am greeting with 'Greetings'" bash -c "hello | grep 'Greetings, $(whoami)'"
-
+check "execute command" bash -c "dpkg-query -l libjemalloc-dev"
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
